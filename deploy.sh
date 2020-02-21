7e5a266149f62301948c984cabc303f688470635
Miquel Oliver <miguel.oliver.maikel@gmail.com>
2020-02-21 23:14:40 +0100

    Deployment almost ready: the problem is that the backend exits if it is run from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,24 @@
-docker network create hab-net
-docker run -d --network hab-net --name proxy proxy-budget
+NETWORK=hab-net
+
+docker network create $NETWORK
+
+# Build images
+cd database
+docker build -t mysql-budget .
+
+cd ../proxy
+docker build -t proxy-budget .
+
+cd ../frontend
+docker build -t frontend-budget .
+
+# Created before the rest to pass tests
+docker run --rm -d --network $NETWORK --name mysql mysql-budget
+
+cd ../backend
+docker build --network $NETWORK -t backend-budget .
+
+# Run containers (backend and proxy ports exported, although not necessary)
+docker run --rm -d --network $NETWORK -p 8081:8081 --name backend backend-budget
+docker run --rm -d --network $NETWORK -p 8082:8082 --name proxy proxy-budget 
+docker run --rm -d --network $NETWORK -p 8080:8080 --name frontend frontend-budget
