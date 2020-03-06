ba7f8d849440d1f7d87b87132bc89b3fbee192cf
Juan Moreno <redtitle@msn.com>
2020-03-06 09:54:49 +0100

    Update BudgetService.js

--- frontend/src/services/BudgetService.js
+++ frontend/src/services/BudgetService.js
@@ -1,32 +1,33 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8081/api/v1'
+const API_URL = 'http://localhost:8081/api/v1';
+const ENDPOINT = '/budgets';
 
 class BudgetService {
 
     retrieveAllBudgets() {
         console.log('GET /budgets')
-        return axios.get(API_URL + "/budgets");
+        return axios.get(API_URL + ENDPOINT);
     }
 
     retrieveBudget(id) {
         console.log('GET /budgets/:id')
-        return axios.get(API_URL + "/budgets/" + id);
+        return axios.get(API_URL + ENDPOINT + "/" + id);
     }
 
     deleteBudget(id) {
         console.log('DELETE /budgets/:id')
-        return axios.delete(API_URL + "/budgets/" + id);
+        return axios.delete(API_URL + ENDPOINT + "/" + id);
     }
 
     updateBudget(id, budget) {
         console.log('POST /budgets/:id')
-        return axios.put(API_URL + "/budgets/" + id, budget);
+        return axios.put(API_URL + ENDPOINT + "/" + id, budget);
     }
 
     createBudget(budget) {
         console.log('POST /budgets')
-        return axios.post(API_URL + "/budgets", budget);
+        return axios.post(API_URL + ENDPOINT, budget);
     }
 }
 
